afb1f44a0e1bf617a7600169a62a28063ed93257
wenxuan27 <wenxuan27@outlook.com>
2020-09-30 01:12:13 -0400

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    private int m_NbProjectiles = 0;
+
+    public int GetNbProjectiles()
+    {
+        return m_NbProjectiles;
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Projectile"))
+        {
+            m_NbProjectiles++;
+            Destroy(other.gameObject); // Or whatever way you want to remove the coin.
+        }
+    }
+}
---
+++ Assets/ass1/Scripts/Gamemanager/Gamemanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class Gamemanager : MonoBehaviour
+{
+    
+    // private int m_NbProjectiles = 0;
+
+    private GameObject playerGameObject;
+    private Player playerScript;
+
+    private Canvas canvas;
+    public TextMeshProUGUI projectilesText;
+    private TextMeshProUGUI gameStateText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.FindGameObjectWithTag("MainCanvas").GetComponent<Canvas>();
+        projectilesText = GameObject.FindGameObjectWithTag("ProjectileText").GetComponent<TextMeshProUGUI>();
+        gameStateText = GameObject.FindGameObjectWithTag("GameStateText").GetComponent<TextMeshProUGUI>();
+        playerGameObject = GameObject.FindWithTag("Player");
+        playerScript = playerGameObject.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectilesText.text = "Projectiles: " + playerScript.GetNbProjectiles();
+    }
+}
