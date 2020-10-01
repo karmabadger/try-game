9d3533942fb57405560564c0f53e367c27b194c3
wenxuan27 <wenxuan27@outlook.com>
2020-09-30 20:48:56 -0400

    projectiles

---
+++ Assets/Platform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject); 
+        }
+        
+    }
+}
