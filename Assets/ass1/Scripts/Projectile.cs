9cf6fc135a9335905457fe9cf851196c87aee9e2
wenxuan27 <wenxuan27@outlook.com>
2020-10-02 03:09:32 -0400

    assets

---
+++ Assets/ass1/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Platform"))
+        {
+            Destroy(other.transform.parent.gameObject);
+            gameObject.SetActive(false);
+            // Destroy(this.gameObject); 
+        } else if (other.CompareTag("Player"))
+        {
+            
+        }else if (other.CompareTag("PlatformWall"))
+        {
+            Destroy(other.transform.parent.gameObject);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            // Destroy(this.gameObject);
+            gameObject.SetActive(false);
+        }
+        
+    }
+}
